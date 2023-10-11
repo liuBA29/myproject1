158c99ac10f7c7a123ecec5aa51bc7fb23d7b818
liuBA <lu-@rambler.ru>
2023-10-11 22:49:30 +0300

    creating first page in our site

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crm_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crm/', index),
 ]
